fa50bc46c3d85b826c3569c8a723c73c000643ae
Amanuel <amanuelwakshum@gmail.com>
2024-02-27 00:10:32 +0300

    completed the home screen

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,11 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:CBE_CLONE/Pages/Accounts.dart';
 import 'package:CBE_CLONE/Pages/HomeContent.dart';
 import 'package:CBE_CLONE/Pages/QRpages.dart';
 import 'package:CBE_CLONE/Pages/Transction.dart';
+import 'package:animated_floating_buttons/widgets/animated_floating_action_button.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:flutter/material.dart';
 import 'package:CBE_CLONE/Widget/NavBar.dart';
 import 'package:CBE_CLONE/constants/App_Colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CBE Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -22,72 +42,80 @@ class _HomeScreenState extends State<HomeScreen> {
     Icon(Icons.receipt_long_sharp, size: 26, color: Colors.white),
   ];
   int index = 0;
-
+  final GlobalKey<AnimatedFloatingActionButtonState> key =
+      GlobalKey<AnimatedFloatingActionButtonState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: NavBar(),
-        bottomNavigationBar: CurvedNavigationBar(
-          index: 0,
-          height: 70.0,
-          backgroundColor: Colors.transparent,
-          buttonBackgroundColor: AppColors.primary,
-          color: AppColors.primary,
-          animationCurve: Curves.easeInOut,
-          animationDuration: const Duration(milliseconds: 300),
-          items: items,
-          onTap: (selectedIndex) {
-            setState(() {
-              index = selectedIndex;
-            });
-          },
-          letIndexChange: (index) => true,
-        ),
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(60),
-          child: AppBar(
-            bottomOpacity: 20,
-            iconTheme: IconThemeData(color: AppColors.primary, size: 27),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'En',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: AppColors.primary,
-                  ),
+      drawer: NavBar(),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 0,
+        height: 70.0,
+        backgroundColor: Colors.transparent,
+        buttonBackgroundColor: AppColors.primary,
+        color: AppColors.primary,
+        animationCurve: Curves.easeInOut,
+        animationDuration: const Duration(milliseconds: 300),
+        items: items,
+        onTap: (selectedIndex) {
+          setState(() {
+            index = selectedIndex;
+          });
+        },
+        letIndexChange: (index) => true,
+      ),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: AppBar(
+          bottomOpacity: 20,
+          iconTheme: IconThemeData(color: AppColors.primary, size: 27),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'En',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: AppColors.primary,
                 ),
-                Container(
-                  color: Colors.transparent,
-                  height: 42,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                  ),
-                  child: const Icon(
-                    Icons.refresh,
-                    color: AppColors.primary,
-                    size: 25,
-                  ),
+              ),
+              Container(
+                color: Colors.transparent,
+                height: 42,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
                 ),
-              ],
-            ),
+                child: const Icon(
+                  Icons.refresh,
+                  color: AppColors.primary,
+                  size: 25,
+                ),
+              ),
+            ],
           ),
         ),
-        body: Container(
-          color: Colors.blueAccent[50],
-          width: double.infinity,
-          height: double.infinity,
-          alignment: Alignment.center,
-          child: getSelectedWidget(index: index),
-        ));
+      ),
+      floatingActionButton: AnimatedFloatingActionButton(
+          //Fab list
+          fabButtons: <Widget>[float1(), float2(), float3()],
+          key: key,
+          colorStartAnimation: AppColors.primary,
+          colorEndAnimation: AppColors.primary,
+          animatedIconData: AnimatedIcons.view_list),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        alignment: Alignment.center,
+        child: getSelectedWidget(index: index),
+      ),
+    );
   }
 
   Widget getSelectedWidget({required int index}) {
     late Widget widget;
     switch (index) {
       case 0:
-        widget = const HomeContent();
+        widget = HomeContent();
         break;
       case 1:
         widget = const QRpages();
@@ -99,9 +127,41 @@ class _HomeScreenState extends State<HomeScreen> {
         widget = const Transcation();
         break;
       default:
-        // Handle cases where index is out of range or not expected.
-        widget = Container(); // Or any other default widget or null if needed.
+        widget = Container();
     }
     return widget;
   }
+}
+
+Widget float1() {
+  return Container(
+    child: FloatingActionButton(
+      onPressed: null,
+      heroTag: "btn1",
+      tooltip: 'First button',
+      child: Icon(Icons.monetization_on_sharp),
+    ),
+  );
+}
+
+Widget float2() {
+  return Container(
+    child: FloatingActionButton(
+      onPressed: null,
+      heroTag: "btn2",
+      tooltip: 'Second button',
+      child: Icon(Icons.qr_code),
+    ),
+  );
+}
+
+Widget float3() {
+  return Container(
+    child: FloatingActionButton(
+      onPressed: null,
+      heroTag: "btn2",
+      tooltip: 'Second button',
+      child: Icon(Icons.receipt_long_outlined),
+    ),
+  );
 }
--- lib/Pages/HomeContent.dart
+++ lib/Pages/HomeContent.dart
@@ -1,8 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:CBE_CLONE/constants/App_Colors.dart';
 
-class HomeContent extends StatelessWidget {
+
+class ServiceItem extends StatelessWidget {
+  final String serviceName;
+  final IconData iconData;
+  final Function()? onTap;
+
+  const ServiceItem({
+    required this.serviceName,
+    required this.iconData,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        elevation: 3,
+        color: Color.fromARGB(255, 255, 255, 255),
+        shadowColor: Colors.grey[50],
+        child: Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(iconData, size: 35, color: AppColors.primary,),
+              SizedBox(height: 10),
+              Text(
+                serviceName,
+                style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500, color: AppColors.primary),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class HomeContent extends StatefulWidget {
   const HomeContent({Key? key}) : super(key: key);
+
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  bool showFullAccountNumber = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,124 +58,230 @@ class HomeContent extends StatelessWidget {
     String formattedDateTime =
         "${_getFormattedDate(now)} ${_getFormattedTime(now)}";
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: size.width * 0.9,
-          height: size.height * 0.282,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 3, 26, 37),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Stack(
-            children: [
-              // Background image
-              Positioned.fill(
-                child: Image.asset(
-                  'assets/worldmap.png',
-                  fit: BoxFit.cover,
-                ),
+    String accountNumber = '1000142402614';
+    String Balance = '20000';
+    List<Map<String, dynamic>> services = [
+      {
+        'name': 'Transfer',
+        'icon': Icons.screen_rotation_alt_rounded,
+        'route': '/',
+      },
+      {
+        'name': 'Utilities',
+        'icon': Icons.cabin_sharp,
+        'route': '/',
+      },
+      {
+        'name': 'Top up',
+        'icon': Icons.mobile_screen_share_sharp,
+        'route': '/',
+      },
+      {
+        'name': 'Help',
+        'icon': Icons.help_center_rounded,
+        'route': '/',
+      },
+      {
+        'name': 'Utility',
+        'icon': Icons.mic_external_on_sharp,
+        'route': '/',
+      },
+      {
+        'name': 'Government',
+        'icon': Icons.screen_rotation_alt_rounded,
+        'route': '/',
+      },
+      {
+        'name': 'Banking',
+        'icon': Icons.account_balance_rounded,
+        'route': '/',
+      },
+      {
+        'name': 'Travel',
+        'icon': Icons.mode_of_travel_rounded,
+        'route': '/',
+      },
+      {
+        'name': 'Pay For',
+        'icon': Icons.payments_outlined,
+        'route': '/',
+      },
+    ];
+    return SafeArea(
+      
+      child: Column(
+      
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: size.width * 1,
+              height: size.height * 0.29,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 3, 26, 37),
+                borderRadius: BorderRadius.circular(10),
               ),
-              Column(
+              child: Stack(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 2.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/Dj6xJ2dX0AADlPv-removebg-preview.png',
-                          height: 80,
-                          width: 80,
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              'Commercial Bank of Ethiopia',
-                              style: TextStyle(
-                                color: AppColors.secondary,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            Text(
-                              'The Bank You can always Rely on!',
-                              style: TextStyle(
-                                color: const Color.fromARGB(255, 253, 233, 164),
-                                fontSize: 10.0,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
+                  Positioned.fill(
+                    child: Image.asset(
+                      'assets/worldmap.png',
+                      fit: BoxFit.cover,
                     ),
-                  ),
-                  Text(
-                    'Balance',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: const Color.fromARGB(255, 233, 231, 231),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Text(
-                          '*****',
-                          style: TextStyle(
-                            fontSize: 26,
-                            color: const Color.fromARGB(255, 233, 231, 231),
-                          ),
-                        ),
-                      ),
-                      Text(
-                        "Br.",
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.w600,
-                          color: const Color.fromARGB(255, 233, 231, 231),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Icon(
-                          Icons.remove_red_eye,
-                          color: Color.fromARGB(255, 233, 231, 231),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
                   ),
                   Column(
                     children: [
-                      Text(
-                        'Saving - 1000******2614',
-                        style:
-                            TextStyle(fontSize: 14, color: AppColors.secondary),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 2.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/Dj6xJ2dX0AADlPv-removebg-preview.png',
+                              height: 80,
+                              width: 80,
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  'Commercial Bank of Ethiopia',
+                                  style: TextStyle(
+                                    color: AppColors.secondary,
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  'The Bank You can always Rely on!',
+                                  style: TextStyle(
+                                    color:
+                                        const Color.fromARGB(255, 253, 233, 164),
+                                    fontSize: 10.0,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                       Text(
-                        formattedDateTime,
+                        'Balance',
                         style: TextStyle(
-                            fontSize: 11,
-                            color: const Color.fromARGB(255, 233, 231, 231)),
+                          fontSize: 14,
+                          color: const Color.fromARGB(255, 233, 231, 231),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            showFullAccountNumber
+                                ? Balance
+                                : MakeHideNumbers('*****', 0,0),
+                            style: TextStyle(
+                              fontSize: 26,
+                              color: const Color.fromARGB(255, 233, 231, 231),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              "Br.",
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.w600,
+                                color: const Color.fromARGB(255, 233, 231, 231),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.remove_red_eye,
+                              color: Color.fromARGB(255, 186, 184, 184),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                showFullAccountNumber = !showFullAccountNumber;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'Saving - ${showFullAccountNumber ? accountNumber : MakeHideNumbers(accountNumber, 4, 4)}',
+                            style: TextStyle(
+                                fontSize: 16, color: AppColors.secondary),
+                          ),
+                          Text(
+                            formattedDateTime,
+                            style: TextStyle(
+                                fontSize: 11,
+                                color: const Color.fromARGB(255, 233, 231, 231)),
+                          )
+                        ],
                       )
                     ],
-                  )
+                  ),
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        )
-      ],
+          Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    'Our Services',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.primary),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Add your GridView here
+          Expanded(
+            child: Container(
+                          color: Colors.white70,
+
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: GridView.builder(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  
+                  gridDelegate:
+                      SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                  itemCount: services.length,
+                  itemBuilder: (context, index) {
+                    return ServiceItem(
+                      serviceName: services[index]['name'],
+                      iconData: services[index]['icon'],
+                      onTap: () {
+                        Navigator.pushNamed(context, services[index]['route']);
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -139,4 +292,11 @@ class HomeContent extends StatelessWidget {
   String _getFormattedTime(DateTime dateTime) {
     return "${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}";
   }
+String MakeHideNumbers(String label, int visibleStart, int visibleEnd) {
+    String MaskedNumber = '*' * (label.length - visibleStart - visibleEnd);
+    return label.substring(0, visibleStart) +
+        MaskedNumber +
+        label.substring(label.length - visibleEnd);
+  }
 }
+
